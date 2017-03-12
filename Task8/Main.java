8f4024f9abd85498973f635c139f2385b72738d1
KirillBelavin <k.a.belavin@gmail.com>
2017-03-12 19:49:26 +0300

    task 8 done

---
+++ Task8/Main.java
@@ -0,0 +1,21 @@
+class Main {
+	public static void main(String[] args) {
+     int a[] = new int[10];
+		a[0] = 10;
+		a[1] = -7;
+		a[2] = 4;
+		a[3] = 12;
+		a[4] = -10;
+		a[5] = 5;
+		a[6] = 2;
+		a[7] = 1;
+		a[8] = 9;
+		a[9] = 2;
+		int sum = 0;
+		  for (int i = 0; i < a.length; i++) {
+		  	if (i % 2 == 0)
+			sum = sum + a[i];
+		}
+		System.out.println("amount =" + sum);
+	}	
+}
